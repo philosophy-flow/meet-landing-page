74c1600a67c00e88780b648f94ca0945e0748466
Jacob S <jacob.g.stewart@gmail.com>
2021-05-06 21:58:17 -0400

    adding modal for download btn

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ![screenshot](./assets/product-landing-page-demo.png)
 
-A mobile first, fully responsive landing page for an app named Meet. The page was built to be fluid and responsive across all viewports; two breakpoints were created to strategically compliment the content. A small amount of JavaScript was used to create a simple scroll button.
+A mobile first, fully responsive landing page for an app named Meet. The page was built to be fluid and responsive across all viewports; two breakpoints were created to strategically compliment the content, and a small amount of JavaScript was used to implement a simple scroll button and download modal. Care was taken to ensure that smooth scrolling and CSS transitions were applied across devices to maximize UX.
 
 [Live Demo](https://philosophy-flow.github.io/meet-landing-page/)
 
--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// handles scroll to about section
 const scrollTarget = document.getElementById('scroll-target');
 const scrollBtn = document.getElementById('scroll-btn');
 scrollBtn.addEventListener('click', () => scrollTarget.scrollIntoView({behavior:'smooth'}));
+
+
+// handles opening modal w/ both download btn
+const modal = document.getElementById('modal');
+const modalBtn1 = document.getElementById('download-btn-1');
+const modalBtn2 = document.getElementById('download-btn-2');
+
+[modalBtn1, modalBtn2].forEach(btn => {
+  btn.addEventListener('click', () => {
+    modal.classList.add('active');
+    document.body.style.overflowY = 'hidden';
+  })
+});
+
+
+// handles closing modal
+const closeBtn = document.getElementById('close-modal-btn');
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('active');
+  document.body.style.overflowY = 'auto';
+})
